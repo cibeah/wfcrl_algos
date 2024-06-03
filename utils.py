7ab3c8e41fefcdd7835993f0b1e2c5897316cf00
cibeah <cibeah.cb@gmail.com>
2024-06-03 19:21:39 +0200

    update utils

--- utils.py
+++ utils.py
@@ -8,15 +8,19 @@ import seaborn as sns
 
 sns.set_theme(style="darkgrid")
 
-
-def plot_env_history(env):
+def get_env_history(env):
     columns = [f"T{i+1}" for i in range(env.num_turbines)]
     yaws = np.c_[[[h["yaw"] for h in env.history[agent]["observation"]]for agent in env.possible_agents]].T
     powers = np.c_[[env.history[agent]["power"] for agent in env.possible_agents]].T
     loads = np.c_[[env.history[agent]["load"] for agent in env.possible_agents]].T
+    rewards = np.c_[[env.history[agent]["reward"] for agent in env.possible_agents]].T
     yaws = pd.DataFrame(yaws, columns=columns)
     powers = pd.DataFrame(powers, columns=columns)
     loads = pd.DataFrame(np.abs(loads).sum(0), columns=columns)
+    return yaws, powers, loads, rewards
+
+def plot_env_history(env):
+    yaws, powers, loads, _ = get_env_history(env)
     fig, ax = plt.subplots(ncols=3, figsize=(15, 5))
     ax0 = sns.lineplot(yaws, ax=ax[0])
     ax1 = sns.lineplot(powers.sum(1), ax=ax[1])
